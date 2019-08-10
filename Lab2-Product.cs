4c2feeaf4ac6d005d667db67e5769696002ba08e
namnvth1809002 <namnvth1809002@fpt.edu.vn>
2019-08-10 17:09:36 +0700

    lab2

---
+++ Lab2-Product.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SESSION_2
+{
+    public class Product
+    {
+        public string id,name;
+        public double price;
+        public string image;
+        public string desc;
+        public int qty;
+        public List <string> gallery = new List<string>();
+        public Product()
+        {
+            
+        }
+
+        public Product(string id, string name, double price, int qty,string image, string desc,List<string> gallery)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.qty = qty;
+            this.image = image;
+            this.desc = desc;
+            this.gallery = gallery;
+        }
+
+        
+        
+        public void getInfor()
+        {
+            int k = 1;
+            Console.WriteLine("ID : "+id);
+                Console.WriteLine("Name : "+name);
+                Console.WriteLine("Price : "+price);
+                Console.WriteLine("Price : " + qty);
+                Console.WriteLine("Image : "+image);
+                Console.WriteLine("Desc: " + desc);
+                Console.WriteLine("Gallery : ");
+                foreach (var p in gallery)
+                {
+                    Console.WriteLine(k+"."+p);
+                    k++;
+                }
+                Console.WriteLine();
+            
+        }
+
+        public void thongBao()
+        {
+            if (qty > 0)
+            {
+                Console.WriteLine("Con hang ");
+            }else
+                Console.WriteLine("Het hang");
+        }
+
+        public void themAnh()
+        {
+            string n;
+            if (gallery.Count > 10)
+            {
+                Console.WriteLine("KHONG THE THEM ANH VI DA DU 10 TAM ! NEU MUON THEM HAY XOA BOT");
+            }
+            else
+            {
+                Console.WriteLine("Nhap anh : ");
+                n = Console.ReadLine();
+                gallery.Add(n);
+            }
+            
+            Console.WriteLine("---------------");
+            
+        }
+
+        public void xoaAnh()
+        {
+            Console.WriteLine("Nhap vi tri can xoa  anh : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < gallery.Count; i++)
+            {
+                if (n == i)
+                {
+                    gallery.RemoveAt(i-1);
+                }
+            }
+            Console.WriteLine();
+        }
+        
+        
+    }
+}
---
+++ Lab2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace SESSION_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Product p = new Product();
+            p.id = "ID-1";
+            p.name = "Tu lanh";
+            p.price = 102.20;
+            p.qty = 10;
+            p.image = "anh_1";
+            p.desc = "Binh thuong";
+            p.gallery.Add("anh_ID-1.png");
+            p.gallery.Add("and_ID-1.2.jpg");
+            int choice;
+            do {            
+                Console.WriteLine("\n-------------------------Menu-------------------------");
+                Console.WriteLine("1.Kiem tra hang");
+                Console.WriteLine("2.Them anh");
+                Console.WriteLine("3.Xoa anh");
+                Console.WriteLine("4.Hien thi thong tin san pham");
+                Console.WriteLine("0.Exit");
+                do {            
+                    Console.WriteLine("Nhập các số để chọn từng chức năng (1/2/3/4/0) : ");
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    if (choice < 0 || choice > 6) {
+                        Console.WriteLine("Nhập sai ! xin hãy nhập lại");
+                    }
+                } while (choice < 0 || choice > 6);           
+                switch (choice){
+                    case 1:
+                        Console.WriteLine("Bạn đã chọn chức năng Kiem tra hang ");
+                        p.thongBao();
+                        break;
+                    case 2 :
+                        Console.WriteLine("Bạn đã chọn chức năng Them anh");
+                        p.themAnh();
+                        break;
+                    case 3 :
+                        Console.WriteLine("Bạn đã chọn chức năng Xoa anh ");
+                        p.xoaAnh();
+                        break;
+                    case 4 :
+                        Console.WriteLine("Bạn đã chọn chức năng Hien thi thong tin san pham :");
+                        Console.WriteLine();
+                        p.getInfor();
+                        break;
+        
+                    case 0 :
+                        Console.WriteLine("Bạn đã chọn chức năng thoát chương trình ! Tạm biệt");
+                        return;
+               
+                }
+            
+            } while (choice != 0);
+
+        }
+    }
+}
